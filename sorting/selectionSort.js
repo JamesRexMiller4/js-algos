2510972e9beb2f6668f7a33a726cc3fd0edafb1a
James Miller <james.rex.miller.4@gmail.com>
2020-08-14 06:22:48 -0600

    Add selectionSort

---
+++ sorting/selectionSort.js
@@ -0,0 +1,24 @@
+const selectionSort = (arr) => {
+  for (let current = 0; current < arr.length - 1; current++){
+    let minIndex = current;
+    for (let i = current + 1; i < arr.length; i++){
+      if (arr[i] < arr[minIndex]) {
+        minIndex = i;
+      }
+    }
+    let temp = arr[current];
+    arr[current] = arr[minIndex];
+    arr[minIndex] = temp;
+    temp = null;
+  }
+  console.log(arr);
+  return arr
+};
+
+
+
+const test = [1, 6, 7, 2, 4, 8, 12, 67, 23, 45, 99, 42];
+const test2 = [-2, 34, -42, -6, 9, 12, 23, 100, 44, -99];
+
+selectionSort(test);
+selectionSort(test2);
